c7591cac4f5f7089b1e3bcebd05c381928446637
Erika <erika.orregog@opitech.com.co>
2024-10-26 11:29:27 -0500

    feat: initialize globes

--- bomberman_model.py
+++ bomberman_model.py
@@ -1,19 +1,28 @@
-from mesa import Agent, Model
+import random
+from mesa import Model
 from mesa.time import RandomActivation
 from mesa.space import MultiGrid
 
 from models.block import Block
 from models.bomberman import Bomberman
+from models.globe import Globe
 from models.metal import Metal
+from models.number_marker import NumberMarker
 
-class BombermanModel(Model):
+class BombermanModel(Model):    
     def __init__(self, width, high, map_file, algorithm="BFS"):
         super().__init__()
-        self.grid = MultiGrid(width, high, True)
+        self.grid_width = width
+        self.grid_height = high
+        self.grid = MultiGrid(width, high, torus=False)
         self.schedule = RandomActivation(self)
         self.algorithm = algorithm
         unique_id_counter = 0  
         self.visited_numbers = {}
+        globe_positions = []
+        self.previous_positions = {}
+        self.map_file = map_file
+        self.running = True  # Bandera para controlar la ejecución del juego
 
         # Crear el mapa y colocar los agentes correspondientes
         for y, row in enumerate(reversed(map_file)):
@@ -41,7 +50,23 @@ class BombermanModel(Model):
                     unique_id_counter += 1
                     self.grid.place_agent(metal, (x, y))
                     self.schedule.add(metal)
+                
+                elif cell == "C_g":
+                    globe_positions.append((x, y))
     
+        for globe_position in globe_positions:
+                globe = Globe(globe_position, self)
+                self.grid.place_agent(globe, globe_position)
+                self.schedule.add(globe)
+
+        if not globe_positions:
+                self.add_globes(1)
+
+    def finish_game(self):
+        """Detiene la ejecución del juego."""
+        self.running = False
+        print("Juego terminado debido a colisión.")
+
     def place_agent_number(self, pos, number):
         print(f"Colocando número {number} en la casilla {pos}")
         print(f"self.visited_numbers: {self.visited_numbers}")
@@ -51,13 +76,21 @@ class BombermanModel(Model):
         self.grid.place_agent(NumberMarker(pos, self, number), pos)
 
     def step(self):
-        # Avanzar en el tiempo
-        self.schedule.step()
+        # Avanzar en el tiempo solo si el juego está activo
+        if self.running:
+            self.schedule.step()
 
-class NumberMarker(Agent):
-    def __init__(self, pos, model, number):
-        super().__init__(pos, model)
-        self.number = number
+    def add_globes(self, count):
+        for _ in range(count):
+            # Buscar una posición válida aleatoria (C)
+            while True:
+                x = random.randrange(self.grid_width)
+                y = random.randrange(self.grid_height)
+                if self.grid.is_cell_empty((x, y)):  # Verificar si es un camino libre
+                    globe = Globe((x, y), self)
+                    self.grid.place_agent(globe, (x, y))
+                    self.schedule.add(globe)
+                    break
 
-    def step(self):
-        pass  # Los números no hacen nada, son solo decorativos
+    def update_previous_position(self, agent, new_position):
+        self.previous_positions[agent] = new_position
